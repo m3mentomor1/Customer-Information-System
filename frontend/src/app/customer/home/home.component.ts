d4da193066bc815f2ece3090ea6154f6bb5dae20
Shain Sahagun <qsaqsahagun@tip.edu.ph>
2025-04-01 13:15:47 +0800

    Changes to frontend: - Added pagination, filtering, & sort to table

--- frontend/src/app/customer/home/home.component.ts
+++ frontend/src/app/customer/home/home.component.ts
@@ -1,10 +1,12 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { Customer } from '../customer';
 import { CustomerService } from '../customer.service';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-home',
@@ -14,6 +16,8 @@ import { CustomerService } from '../customer.service';
     MatInputModule,
     MatButtonModule,
     MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
@@ -21,6 +25,7 @@ import { CustomerService } from '../customer.service';
 export class HomeComponent implements AfterViewInit {
   displayedColumns: string[] = ['id', 'name', 'email', 'address'];
   customers: Customer[] = [];
+  filteredCustomers: Customer[] = [];
   dataSource = new MatTableDataSource<Customer>();
   customer: Customer = {
     id: 0,
@@ -29,12 +34,26 @@ export class HomeComponent implements AfterViewInit {
     address: '',
   };
 
+  @ViewChild(MatSort) sort: any;
+  @ViewChild(MatPaginator) paginator: any;
   constructor(private customerService: CustomerService) {}
 
   ngAfterViewInit(): void {
     this.customerService.fetchAllCustomers().subscribe((data) => {
       this.customers = data;
       this.dataSource = new MatTableDataSource<Customer>(data);
+      this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
     });
+  }
+
+  filterCustomers(input: string) {
+    this.filteredCustomers = this.customers.filter(
+      (item) =>
+        item.name.toLowerCase().includes(input.toLowerCase()) ||
+        item.email.toLowerCase().includes(input.toLowerCase()) ||
+        item.address.toLowerCase().includes(input.toLowerCase())
+    );
+    this.dataSource = new MatTableDataSource<Customer>(this.filteredCustomers);
   }
 }
